fa4df46f13649c2e8622b5edb724ae847aa0a11e
Suyash Pandey <sayashpandey30@gmail.com>
2024-07-01 20:17:32 +0530

    commit 7

---
+++ 07.DragNdrop/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from "react";
+import {Reorder} from 'framer-motion'
+
+// Framer Motion
+
+export default function App() {
+
+  const [listItems, setListItems] = useState(["Aman","Suyash","Yashi"]);
+
+  return (
+    <Reorder.Group axis="y" onReorder={setListItems} values={listItems}>
+      {listItems.map((el) => (
+        <Reorder.Item key={el} value={el}>
+          <span>{el}</span>
+        </Reorder.Item>
+      ))}
+    </Reorder.Group>
+  );
+}
